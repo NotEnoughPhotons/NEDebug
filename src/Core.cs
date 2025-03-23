dd8b46937e7e2ff9f40ec9c8c6c76266eb408425
adamdev <adamdevofficial@protonmail.com>
2025-03-23 12:02:38 -0400

    Headset view rendering and example code

--- src/Core.cs
+++ src/Core.cs
@@ -1,7 +1,12 @@
-﻿using MelonLoader;
-using BoneLib;
+﻿using System.Reflection;
+
+using MelonLoader;
+
 using UnityEngine;
+
 using Il2CppSLZ.Marrow;
+
+using BoneLib;
 
 [assembly: MelonInfo(typeof(NEP.NEDebug.Core), "NEDebug", "1.0.0", "Not Enough Photons: adamdev", null)]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
@@ -11,12 +16,16 @@ namespace NEP.NEDebug
     public class Core : MelonMod
     {
         internal static MelonLogger.Instance m_logger;
+        internal static Material m_visMaterial;
 
         public Transform playerTorso;
 
         public override void OnInitializeMelon()
         {
             m_logger = Melon<Core>.Logger;
+            string packDir = HelperMethods.IsAndroid() ? "nedraw_shaders_quest.pack" : "nedraw_shaders.pack";
+            AssetBundle bundle = HelperMethods.LoadEmbeddedAssetBundle(Assembly.GetExecutingAssembly(), "NEP.NEDebug.Resources." + packDir);
+            m_visMaterial = bundle.LoadPersistentAsset<Material>("VisDraw");
             Hooking.OnLevelLoaded += OnLevelLoaded;
         }
 
@@ -27,6 +36,7 @@ namespace NEP.NEDebug
 
         public override void OnUpdate()
         {
+#if DEBUG
             if (!Player.HandsExist)
             {
                 return;
@@ -37,7 +47,17 @@ namespace NEP.NEDebug
             Quaternion rot = rig.torso.transform.rotation;
             NEDebug.DrawBox(pos, rot, Color.blue);
             NEDebug.DrawLine(pos, Vector3.zero, Color.red);
+
+            Transform lhand = rig.leftHand.transform;
+
+            NEDebug.DrawLine(lhand.position, lhand.position + lhand.right * 0.1f, Color.red);
+            NEDebug.DrawLine(lhand.position, lhand.position + lhand.forward * 0.1f, Color.blue);
+            NEDebug.DrawLine(lhand.position, lhand.position + lhand.up * 0.1f, Color.green);
+
+            Ray ray = new Ray(pos, rig.torso.transform.forward * 0.5f);
+            NEDebug.DrawRay(ray, Color.yellow);
             // NEDebug.DrawBox(Vector3.zero, Quaternion.identity, Color.red);
+#endif
         }
     }
 }
--- src/NEDebugVisualizer.cs
+++ src/NEDebugVisualizer.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using BoneLib;
+using UnityEngine;
 using UnityEngine.Rendering;
 
 namespace NEP.NEDebug
@@ -12,14 +13,27 @@ namespace NEP.NEDebug
 
         private Stack<NEDrawCommand> m_commands;
         private Material m_material;
+        private Camera m_vrCamera;
 
         private void Awake()
         {
-            Shader shader = Shader.Find("Hidden/Internal-Colored");
-            m_material = new Material(shader);
-            m_material.hideFlags = HideFlags.HideAndDontSave;
+            if (!Core.m_visMaterial)
+            {
+                NEDebug.Logger.Error("Failed to get visual material shader!");
+            }
+            else
+            {
+                m_material = new Material(Core.m_visMaterial);
+                m_material.SetInt("_SrcBlend", (int)BlendMode.SrcAlpha);
+                m_material.SetInt("_DstBlend", (int)BlendMode.OneMinusSrcAlpha);
+                m_material.SetInt("_Cull", (int)CullMode.Off);
+                m_material.SetInt("_ZWrite", 0);
+            }
+
             m_commands = new Stack<NEDrawCommand>();
             m_urpRenderCallback += OnEndContextRendering;
+            m_vrCamera = Player.ControllerRig.cameras[0];
+            NEDebug.Logger.Log($"Got {m_vrCamera.name} as the main NEDebug drawing camera");
         }
 
         private void OnDestroy()
@@ -40,13 +54,18 @@ namespace NEP.NEDebug
 
         private void DebugDraw()
         {
-            m_material.SetPass(0);
+            if (!m_material)
+            {
+                return;
+            }
+
 
             while (m_commands.Count > 0)
             {
                 NEDrawCommand command = m_commands.Pop();
 
                 GL.PushMatrix();
+                m_material.SetPass(0);
                 GL.MultMatrix(command.transform);
 
                 GL.Begin(command.drawType);
