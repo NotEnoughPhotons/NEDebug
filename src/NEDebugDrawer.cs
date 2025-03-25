e9380cb88c1aecf7da140f0014982d2b1c50968a
adamdev <adamdevofficial@protonmail.com>
2025-03-24 22:27:58 -0400

    Reimplemented/renamed NEDebugDrawer

---
+++ src/NEDebugDrawer.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace NEP.NEDebug
+{
+    [MelonLoader.RegisterTypeInIl2Cpp]
+    internal sealed class NEDebugDrawer : MonoBehaviour
+    {
+        public NEDebugDrawer(System.IntPtr ptr) : base(ptr) { }
+
+        private Action<ScriptableRenderContext, Il2CppSystem.Collections.Generic.List<Camera>> m_urpRenderCallback;
+
+        private Stack<NEDrawCommand> m_commands;
+        private Material m_material;
+        private Camera m_vrCamera;
+
+        private void Awake()
+        {
+            if (!Core.m_visMaterial)
+            {
+                NEDebug.Logger.Error("Failed to get visual material shader!");
+            }
+            else
+            {
+                m_material = new Material(Core.m_visMaterial);
+            }
+
+            m_commands = new Stack<NEDrawCommand>();
+            m_urpRenderCallback += OnEndContextRendering;
+            m_vrCamera = BoneLib.Player.ControllerRig.cameras[0];
+            NEDebug.Logger.Log($"Got {m_vrCamera.name} as the main NEDebug drawing camera");
+        }
+
+        private void OnDestroy()
+        {
+            m_urpRenderCallback -= OnEndContextRendering;
+            m_commands.Clear();
+            m_commands = null;
+        }
+
+        private void OnEndContextRendering(ScriptableRenderContext context, Il2CppSystem.Collections.Generic.List<Camera> cameras)
+        {
+            DebugDraw();
+        }
+
+        private void OnRenderObject()
+        {
+            DebugDraw();
+        }
+
+        private void DebugDraw()
+        {
+            if (!m_material)
+            {
+                return;
+            }
+
+            while (m_commands.Count > 0)
+            {
+                NEDrawCommand command = m_commands.Pop();
+
+                GL.PushMatrix();
+                m_material.SetPass(0);
+
+                GL.MultMatrix(command.transform);
+
+                GL.Begin(command.drawType);
+
+                GL.Color(command.color);
+
+                for (int i = 0; i < command.positions.Count; i++)
+                {
+                    GL.Vertex(command.positions[i]);
+                }
+
+                GL.End();
+
+                GL.PopMatrix();
+            }
+
+            m_commands.Clear();
+        }
+
+        internal void PushCommand(NEDrawCommand command)
+        {
+            m_commands.Push(command);
+        }
+    }
+}
