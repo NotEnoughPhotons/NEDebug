c69e45dd2a6462699bbbe59774d2b77df0b49c30
adamdev <adamdevofficial@protonmail.com>
2025-03-24 22:27:42 -0400

    NEDebug class changes
    - Moved all drawing functions from NEDebugVisualizer to NEDebug for simplification
    - Reimplemented disc drawing code to be more appealing to look at
    - Added DrawCylinder
    - Added a WIP function, DrawSphere (its kinda hacky)

--- src/NEDebug.cs
+++ src/NEDebug.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Il2CppSystem.Net;
+using UnityEngine;
 
 namespace NEP.NEDebug
 {
@@ -22,127 +23,191 @@ namespace NEP.NEDebug
             }
         }
 
-        internal static NEDebugVisualizer m_instance;
+        internal static NEDebugDrawer m_drawer;
 
         public static void DrawLine(Vector3 p1, Vector3 p2, Color color)
         {
-            if (m_instance == null)
+            if (!m_drawer)
             {
                 return;
             }
 
-            m_instance.DrawLine(p1, p2, color);
+            NEDrawCommand command = new NEDrawCommand();
+            command.transform = Matrix4x4.identity;
+            command.drawType = GL.LINE_STRIP;
+            command.positions = [p1, p2];
+
+            command.color = color;
+            m_drawer.PushCommand(command);
         }
 
         public static void DrawRay(Ray ray, Color color)
         {
-            if (m_instance == null)
-            {
-                return;
-            }
-
-            m_instance.DrawLine(ray.origin, ray.origin + ray.direction, color);
-        }
-
-        public static void DrawPlane(Vector3 position, Color color)
-        {
-            if (m_instance == null)
-            {
-                return;
-            }
-
-            m_instance.DrawPlane(position, Quaternion.identity, Vector3.one, color);
-        }
-
-        public static void DrawPlane(Vector3 position, Vector3 scale, Color color)
-        {
-            if (m_instance == null)
-            {
-                return;
-            }
-
-            m_instance.DrawPlane(position, Quaternion.identity, scale, color);
-        }
-
-        public static void DrawPlane(Vector3 position, Quaternion rotation, Color color)
-        {
-            if (m_instance == null)
-            {
-                return;
-            }
-
-            m_instance.DrawPlane(position, rotation, Vector3.one, color);
+            DrawLine(ray.origin, ray.origin + ray.direction, color);
         }
 
         public static void DrawPlane(Vector3 position, Quaternion rotation, Vector3 scale, Color color)
         {
-            if (m_instance == null)
+            if (!m_drawer)
             {
                 return;
             }
 
-            m_instance.DrawPlane(position, rotation, scale, color);
-        }
+            NEDrawCommand command = new NEDrawCommand();
+            command.transform = Matrix4x4.identity;
+            command.transform *= Matrix4x4.TRS(position, rotation, scale);
+            command.drawType = GL.LINE_STRIP;
 
-        public static void DrawBox(Vector3 position, Color color)
-        {
-            if (m_instance == null)
-            {
-                return;
-            }
+            command.positions = [
+                Vector3.up * -0.5f + new Vector3(0, 0, 0), Vector3.up * -0.5f + new Vector3(1, 0, 0),
+                Vector3.up * -0.5f + new Vector3(1, 0, 0), Vector3.up * -0.5f + new Vector3(1, 1, 0),
+                Vector3.up * -0.5f + new Vector3(1, 1, 0), Vector3.up * -0.5f + new Vector3(0, 1, 0),
+                Vector3.up * -0.5f + new Vector3(0, 1, 0), Vector3.up * -0.5f + new Vector3(0, 0, 0)];
 
-            m_instance.DrawBox(position, Quaternion.identity, Vector3.one, color);
-        }
-
-        public static void DrawBox(Vector3 position, Quaternion rotation, Color color)
-        {
-            if (m_instance == null)
-            {
-                return;
-            }
-
-            m_instance.DrawBox(position, rotation, Vector3.one, color);
+            command.color = color;
+            m_drawer.PushCommand(command);
         }
 
         public static void DrawBox(Vector3 position, Quaternion rotation, Vector3 scale, Color color)
         {
-            if (m_instance == null)
+            if (!m_drawer)
             {
                 return;
             }
 
-            m_instance.DrawBox(position, rotation, scale, color);
+            NEDrawCommand command = new NEDrawCommand();
+            command.transform = Matrix4x4.identity;
+            command.transform *= Matrix4x4.TRS(position, rotation, scale);
+            command.drawType = GL.LINE_STRIP;
+
+            command.positions = [
+                Vector3.one * -0.5f + new Vector3(0, 0, 0), Vector3.one * -0.5f + new Vector3(1, 0, 0),
+                Vector3.one * -0.5f + new Vector3(1, 0, 0), Vector3.one * -0.5f + new Vector3(1, 1, 0),
+                Vector3.one * -0.5f + new Vector3(1, 1, 0), Vector3.one * -0.5f + new Vector3(0, 1, 0),
+                Vector3.one * -0.5f + new Vector3(0, 1, 0), Vector3.one * -0.5f + new Vector3(0, 0, 0),
+                Vector3.one * -0.5f + new Vector3(0, 0, 1), Vector3.one * -0.5f + new Vector3(1, 0, 1),
+                Vector3.one * -0.5f + new Vector3(1, 0, 1), Vector3.one * -0.5f + new Vector3(1, 1, 1),
+                Vector3.one * -0.5f + new Vector3(1, 1, 1), Vector3.one * -0.5f + new Vector3(0, 1, 1),
+                Vector3.one * -0.5f + new Vector3(0, 1, 1), Vector3.one * -0.5f + new Vector3(0, 0, 1),
+                Vector3.one * -0.5f + new Vector3(0, 0, 0), Vector3.one * -0.5f + new Vector3(0, 0, 1),
+                Vector3.one * -0.5f + new Vector3(0, 0, 1), Vector3.one * -0.5f + new Vector3(0, 1, 1),
+                Vector3.one * -0.5f + new Vector3(0, 1, 1), Vector3.one * -0.5f + new Vector3(0, 1, 0),
+                Vector3.one * -0.5f + new Vector3(0, 1, 0), Vector3.one * -0.5f + new Vector3(0, 0, 0),
+                Vector3.one * -0.5f + new Vector3(1, 0, 0), Vector3.one * -0.5f + new Vector3(1, 0, 1),
+                Vector3.one * -0.5f + new Vector3(1, 0, 1), Vector3.one * -0.5f + new Vector3(1, 1, 1),
+                Vector3.one * -0.5f + new Vector3(1, 1, 1), Vector3.one * -0.5f + new Vector3(1, 1, 0),
+                Vector3.one * -0.5f + new Vector3(1, 1, 0), Vector3.one * -0.5f + new Vector3(1, 0, 0)];
+
+            command.color = color;
+            m_drawer.PushCommand(command);
         }
 
-#if DEBUG
-        // TODO: Reimplement so I can reuse this for cylinders and spheres
-        public static void DrawDisc(Vector3 position)
+        public static void DrawDisc(Vector3 position, float radius = 1.0f)
         {
-            if (m_instance == null)
+            DrawDisc(position, Quaternion.identity, radius);
+        }
+
+        public static void DrawDisc(Vector3 position, Quaternion rotation, float radius = 1.0f)
+        {
+            if (!m_drawer)
             {
                 return;
             }
+
+            NEDrawCommand command = new NEDrawCommand();
+            command.positions = new List<Vector3>();
+            command.transform = Matrix4x4.identity;
+            command.transform *= Matrix4x4.TRS(position, rotation, Vector3.one);
+            command.drawType = GL.LINE_STRIP;
 
             int sides = 32;
 
             for (int i = 0; i < sides + 1; i++)
             {
                 float a = i / (float)sides;
-                float angle = a * Mathf.PI * 2;
+                float angleA = a * Mathf.PI * 2f;
 
-                m_instance.DrawLine(position + new Vector3(0f, 0f, 0f), position + new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)), Color.white);
-                m_instance.DrawLine(position + new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)), position + new Vector3(0f, 0f, 0f), Color.white);
+                float b = (i + 1) / (float)sides;
+                float angleB = b * Mathf.PI * 2f;
+
+                Vector3 initial = new Vector3(Mathf.Cos(angleA), 0f, Mathf.Sin(angleA));
+                Vector3 next = new Vector3(Mathf.Cos(angleB), 0f, Mathf.Sin(angleB));
+
+                command.positions.Add(position + initial * radius);
+                command.positions.Add(position + next * radius);
             }
+
+            command.color = Color.white;
+            m_drawer.PushCommand(command);
         }
+
+        public static void DrawCylinder(Vector3 position, float height = 1.0f, float radius = 1.0f)
+        {
+            if (!m_drawer)
+            {
+                return;
+            }
+
+            NEDrawCommand command = new NEDrawCommand();
+            command.positions = new List<Vector3>();
+            command.transform = Matrix4x4.identity;
+            command.transform *= Matrix4x4.TRS(position, Quaternion.identity, Vector3.one);
+            command.drawType = GL.LINES;
+
+            DrawDisc(position, radius);
+            DrawDisc(position + Vector3.up * height * 0.5f, radius);
+
+            for (int i = 0; i < 4; i++)
+            {
+                float a = i / (float)4;
+                float angle = a * Mathf.PI * 2f;
+
+                Vector3 p1 = new Vector3(radius * Mathf.Cos(angle), 0f, radius * Mathf.Sin(angle));
+                Vector3 p2 = new Vector3(radius * Mathf.Cos(angle), height, radius * Mathf.Sin(angle));
+
+                command.positions.Add(position + p1);
+                command.positions.Add(position + p2);
+            }
+
+            command.color = Color.white;
+            m_drawer.PushCommand(command);
+        }
+
+        public static void DrawSphere(Vector3 position, float radius = 1.0f)
+        {
+#if DEBUG
+            if (!m_drawer)
+            {
+                return;
+            }
+
+            NEDrawCommand command = new NEDrawCommand();
+            command.positions = new List<Vector3>();
+            // command.transform = Matrix4x4.identity;
+            // command.transform *= Matrix4x4.TRS(position, Quaternion.identity, Vector3.one);
+            command.drawType = GL.LINE_STRIP;
+
+            // TODO: This is a HACK!
+            // For some reason it won't look at the world right axis.
+            // The fix for now is to rotate it by 1 radian on the X axis, and then rotate -
+            // the Y axis forcefully.
+            // Could be because of a bad matrix multiplication.
+            DrawDisc(position, Quaternion.LookRotation(Vector3.right + (Vector3.up * 1000f)), radius);
+            DrawDisc(position, Quaternion.LookRotation(Vector3.up), radius);
+            DrawDisc(position, Quaternion.LookRotation(Vector3.forward), radius);
+
+            command.color = Color.white;
+            m_drawer.PushCommand(command);
 #endif
+        }
 
         internal static void Initialize()
         {
-            if (m_instance == null)
+            if (!m_drawer)
             {
-                GameObject instanceObject =  new GameObject("NEDebug");
+                GameObject instanceObject = new GameObject("NEDebug");
                 instanceObject.hideFlags = HideFlags.DontUnloadUnusedAsset;
-                m_instance = instanceObject.AddComponent<NEDebugVisualizer>();
+                m_drawer = instanceObject.AddComponent<NEDebugDrawer>();
             }
         }
 
