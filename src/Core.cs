191045c9bf12301c67eb4f23c02a16ac7f8907b2
adamdev <adamdevofficial@protonmail.com>
2025-04-05 23:02:40 -0400

    Removed OnLevelLoaded callback and updated version

--- src/Core.cs
+++ src/Core.cs
@@ -6,12 +6,12 @@ using UnityEngine;
 
 using BoneLib;
 
-[assembly: MelonInfo(typeof(NEP.NEDebug.Core), "NEDebug", "0.0.3", "Not Enough Photons: adamdev", null)]
+[assembly: MelonInfo(typeof(NEP.NEDebug.Core), "NEDebug", "0.0.4", "Not Enough Photons: adamdev", null)]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
 
 namespace NEP.NEDebug
 {
-    public class Core : MelonMod
+    internal class Core : MelonMod
     {
         internal static MelonLogger.Instance m_logger;
         internal static Material m_visMaterial;
@@ -22,14 +22,9 @@ namespace NEP.NEDebug
             string packDir = HelperMethods.IsAndroid() ? "nedraw_shaders_quest.pack" : "nedraw_shaders.pack";
             AssetBundle bundle = HelperMethods.LoadEmbeddedAssetBundle(Assembly.GetExecutingAssembly(), "NEP.NEDebug.Resources." + packDir);
             m_visMaterial = bundle.LoadPersistentAsset<Material>("VisDraw");
-            Hooking.OnLevelLoaded += OnLevelLoaded;
+            Hooking.OnLevelLoaded += (info) => NEDraw.Initialize();
         }
-
-        public void OnLevelLoaded(LevelInfo info)
-        {
-            NEDraw.Initialize();
-        }
-
+        
         public override void OnUpdate()
         {
 #if DEBUG
