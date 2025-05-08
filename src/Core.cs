a5e8ba6bc88ddefcad24b12ccd7d73b2b8f36f02
adamdev <adamdevofficial@protonmail.com>
2025-05-08 10:15:24 -0400

    In-game console for commands

--- src/Core.cs
+++ src/Core.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 using BoneLib;
 
 using NEP.NEDebug.Console;
+using UnityEngine.EventSystems;
 
 [assembly: MelonInfo(typeof(NEP.NEDebug.Core), "NEDebug", "0.0.4", "Not Enough Photons: adamdev", null)]
 [assembly: MelonGame("Stress Level Zero", "BONELAB")]
@@ -18,19 +19,42 @@ namespace NEP.NEDebug
         internal static MelonLogger.Instance m_logger;
         internal static Material m_visMaterial;
 
+        internal static GameObject m_consoleObject;
+        
         public override void OnInitializeMelon()
         {
             m_logger = Melon<Core>.Logger;
-            NEConsole.SweepAssembly();
             string packDir = HelperMethods.IsAndroid() ? "nedraw_shaders_quest.pack" : "nedraw_shaders.pack";
             AssetBundle bundle = HelperMethods.LoadEmbeddedAssetBundle(Assembly.GetExecutingAssembly(), "NEP.NEDebug.Resources." + packDir);
             m_visMaterial = bundle.LoadPersistentAsset<Material>("VisDraw");
-            Hooking.OnLevelLoaded += (info) => NEDraw.Initialize();
+            m_consoleObject = bundle.LoadPersistentAsset<GameObject>("ConsoleBar");
+            NEConsole.Compatibility.SetHarmonyInstance(HarmonyInstance);
+            NEConsole.Compatibility.FlatPlayerSetup();
+            Hooking.OnLevelLoaded += (info) =>
+            {
+                NEDraw.Initialize();
+                NEConsole.Initialize();
+            };
         }
 
         public override void OnDeinitializeMelon()
         {
             NEDraw.UnInitialize();
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                NEConsole.ShowConsole();
+                NEConsole.FocusConsole();
+            }
+        }
+
+        [NEConsoleCommand("testfunction")]
+        public static void TestFunction()
+        {
+            Melon<Core>.Logger.Msg("Hello world!");
         }
     }
 }
--- src/NEConsole.cs
+++ src/NEConsole.cs
@@ -1,11 +1,117 @@
 ﻿using System.Reflection;
 
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+using Il2CppTMPro;
+
+using MelonLoader;
+using HarmonyLib;
+
 namespace NEP.NEDebug.Console
 {
     public static class NEConsole
     {
-        public static Dictionary<string, MethodInfo> Commands = new Dictionary<string, MethodInfo>();
+        internal static class Compatibility
+        {
+            private static HarmonyLib.Harmony m_harmony;
+            
+            private static Assembly m_flatPlayerAssembly;
+            private static MethodInfo m_flatPlayerUpdate;
 
+            internal static void SetHarmonyInstance(HarmonyLib.Harmony instance)
+            {
+                m_harmony = instance;
+            }
+            
+            internal static void FlatPlayerSetup()
+            {
+                IEnumerable<MelonAssembly> assemblies = MelonAssembly.LoadedAssemblies;
+                
+                foreach (MelonAssembly assembly in assemblies)
+                {
+                    if (assembly.Assembly.GetName().Name == "FlatPlayer")
+                    {
+                        m_flatPlayerAssembly = assembly.Assembly;
+                        break;
+                    }
+                }
+
+                if (m_flatPlayerAssembly == null)
+                {
+                    Core.m_logger.Warning("Couldn't find the FlatPlayer mod!");
+                    return;
+                }
+
+                Type[] types = m_flatPlayerAssembly.GetTypes();
+                foreach (var type in types)
+                {
+                    if (type.FullName == "FlatPlayer.FlatBooter")
+                    {
+                        object singleton = type.GetField("Instance", BindingFlags.Public | BindingFlags.Static).GetValue(null);
+
+                        if (singleton == null)
+                        {
+                            Core.m_logger.Error("Couldn't find singleton of FlatPlayer!");
+                            continue;
+                        }
+
+                        m_flatPlayerUpdate = singleton.GetType().GetMethod("OnLateUpdate", BindingFlags.Public | BindingFlags.Instance);
+                        Core.m_logger.Msg("Found the FlatPlayer update method!");
+                        break;
+                    }
+                }
+                
+                PatchFlatPlayer();
+            }
+
+            internal static void PatchFlatPlayer()
+            {
+                if (m_flatPlayerAssembly == null || m_flatPlayerUpdate == null)
+                {
+                    return;
+                }
+
+                MethodInfo originalMethodInfo = m_flatPlayerUpdate;
+                MethodInfo targetMethodInfo = typeof(Compatibility).GetMethod("FlatPlayer_OnLateUpdate_Prefix", BindingFlags.NonPublic | BindingFlags.Static);
+
+                m_harmony.Patch(originalMethodInfo, prefix: new HarmonyMethod(targetMethodInfo));
+            }
+
+            private static bool FlatPlayer_OnLateUpdate_Prefix()
+            {
+                if (m_consoleGo == null)
+                {
+                    return true;
+                }
+
+                if (m_showConsole)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+        
+        public static Dictionary<string, MethodInfo> Commands = new();
+
+        private static GameObject m_consoleGo;
+        private static TMP_InputField m_consoleInput;
+        private static EventSystem m_eventSystem;
+
+        private static bool m_showConsole;
+
+        internal static void Initialize()
+        {
+            m_consoleGo = GameObject.Instantiate(Core.m_consoleObject);
+            m_consoleGo.SetActive(false);
+            m_consoleInput = m_consoleGo.transform.Find("Console/InputField").GetComponent<TMP_InputField>();
+            m_eventSystem = m_consoleGo.GetComponent<EventSystem>();
+            m_consoleInput.onSubmit.AddListener(new Action<string>((input) => { Execute(input); }));
+            SweepAssembly();
+        }
+        
         internal static void SweepAssembly()
         {
             Assembly assembly = Assembly.GetExecutingAssembly();
@@ -21,7 +127,7 @@ namespace NEP.NEDebug.Console
                         continue;
                     }
                     
-                    Commands.Add(command.Command, method);
+                    AddCommand(command.Command, method);
                 }
             }
         }
@@ -40,6 +146,19 @@ namespace NEP.NEDebug.Console
                     method.Invoke(null, null);
                 }
             }
+        }
+
+        public static void ShowConsole()
+        {
+            m_showConsole = !m_showConsole;
+            Cursor.visible = m_showConsole;
+            m_consoleGo.SetActive(m_showConsole);
+            EventSystem.current = m_eventSystem;
+        }
+
+        public static void FocusConsole()
+        {
+            m_consoleInput.Select();
         }
     }
 }
