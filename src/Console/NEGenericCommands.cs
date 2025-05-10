02b4a03a1fa22949d54ea6e9dfa0a218356136b0
adamdev <adamdevofficial@protonmail.com>
2025-05-10 13:32:00 -0400

    Added basic commands

---
+++ src/Console/NEGenericCommands.cs
@@ -0,0 +1,35 @@
+﻿using Il2CppSLZ.Marrow;
+using UnityEngine;
+
+namespace NEP.NEDebug.Console
+{
+    internal static class NEGenericCommands
+    {
+        [NEConsoleCommand("kill")]
+        public static void KillPlayer()
+        {
+            RigManager rig = BoneLib.Player.RigManager;
+            rig.health.TAKEDAMAGE(-10000f);
+        }
+        
+        [NEConsoleCommand("getpos")]
+        public static void GetPlayerPosition()
+        {
+            RigManager rig = BoneLib.Player.RigManager;
+            Vector3 position = rig.transform.position;
+            NELog.Log($"Player position: ({position.x}, {position.y}, {position.z})");
+        }
+        
+        [NEConsoleCommand("setpos")]
+        public static void SetPlayerPosition(float x, float y, float z)
+        {
+            BoneLib.Player.RigManager.Teleport(new Vector3(x, y, z));
+        }
+        
+        [NEConsoleCommand("quit")]
+        public static void QuitGame()
+        {
+            Application.Quit();
+        }
+    }
+}
